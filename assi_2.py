830276e8aa5ead4918cb5856d6956baeec80f155
davidshvn <141252667+davidshvn@users.noreply.github.com>
2025-05-24 14:29:23 +0000

    dropdown

--- assi_2.py
+++ assi_2.py
@@ -7,8 +7,8 @@ from streamlit_folium import st_folium
 CENTER_START = [-33.8688, 151.2093]
 
 # Sydney bounding box
-LAT_MIN, LAT_MAX = -34.10, -33.50
-LON_MIN, LON_MAX = 150.50, 151.40
+LAT_MIN, LAT_MAX = -34.25, -33.35
+LON_MIN, LON_MAX = 150.75, 151.15
 
 # Initialize session state
 if "markers" not in st.session_state:
@@ -22,47 +22,48 @@ if "zoom" not in st.session_state:
 
 st.title("Random Market Generator in NSW")
 
-# Input: how many random markers?
-num_markers = st.number_input("How many random markers to add?", min_value=1, max_value=50, value=1)
+markets = ["92", "94", "97"]
+default_index = markets.index("92")
+selected_market = st.selectbox("Fuel", markets, index=default_index)
+
 if st.button("Add Random Markers"):
-    for i in range(num_markers):
+    
+    lat = random.uniform(LAT_MIN, LAT_MAX)
+    lon = random.uniform(LON_MIN, LON_MAX)
+    number = random.randint(100, 999)
 
-        lat = random.uniform(LAT_MIN, LAT_MAX)
-        lon = random.uniform(LON_MIN, LON_MAX)
-        number = random.randint(100, 999)
+    # Custom image + number marker
+    image_url = "https://upload.wikimedia.org/wikipedia/en/thumb/e/e8/Shell_logo.svg/150px-Shell_logo.svg.png"
+    html = f"""
+    <div style="position: relative; width: 40px; height: 40px;">
+        <img src="{image_url}" style="width: 100%; height: 100%; border-radius: 50%;">
+        <div style="
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            font-size: 14px;
+            font-weight: bold;
+            color: white;
+            text-shadow:
+            -1px -1px 0 black,
+            1px -1px 0 black,
+            -1px  1px 0 black,
+            1px  1px 0 black;
+        ">{number}</div>
+    </div>
+    """
 
-        # Custom image + number marker
-        image_url = "https://upload.wikimedia.org/wikipedia/en/thumb/e/e8/Shell_logo.svg/150px-Shell_logo.svg.png"
-        html = f"""
-        <div style="position: relative; width: 40px; height: 40px;">
-            <img src="{image_url}" style="width: 100%; height: 100%; border-radius: 50%;">
-            <div style="
-                position: absolute;
-                top: 50%;
-                left: 50%;
-                transform: translate(-50%, -50%);
-                font-size: 14px;
-                font-weight: bold;
-                color: white;
-                text-shadow:
-                -1px -1px 0 black,
-                1px -1px 0 black,
-                -1px  1px 0 black,
-                1px  1px 0 black;
-            ">{number}</div>
-        </div>
-        """
-
-        marker = folium.Marker(
-            location=[lat, lon],
-            icon=folium.DivIcon(
-                icon_size=(40, 40),
-                icon_anchor=(20, 20),
-                html=html
-            ),
-            popup=f"Market #{number}"
-        )
-        st.session_state["markers"].append(marker)
+    marker = folium.Marker(
+        location=[lat, lon],
+        icon=folium.DivIcon(
+            icon_size=(40, 40),
+            icon_anchor=(20, 20),
+            html=html
+        ),
+        popup=f"Market #{number}"
+    )
+    st.session_state["markers"].append(marker)
 
 # Draw the map
 m = folium.Map(location=st.session_state["center"], zoom_start=st.session_state["zoom"])
